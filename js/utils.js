4163f67661ee0b70848716f7198c55ac34ec19f1
Sateesh Kumar Pikkili <163013785+Psk2024@users.noreply.github.com>
2026-01-16 13:46:13 +0530

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,17 @@
+// js/utils.js
+export function debounce(fn, delay = 300) {
+  let t;
+  return (...args) => {
+    clearTimeout(t);
+    t = setTimeout(() => fn(...args), delay);
+  };
+}
 
+export function highlight(text, term) {
+  if (!term) return text;
+  const safe = term.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  return text.replace(
+    new RegExp(`(${safe})`, "gi"),
+    `<mark>$1</mark>`
+  );
+}
