b652d1a292dd24af3a0105c9eb50c5a7b4d12b7a
Sateesh Kumar Pikkili <163013785+Psk2024@users.noreply.github.com>
2026-01-16 13:45:13 +0530

    Update dataService.js

--- js/dataService.js
+++ js/dataService.js
@@ -1,1 +1,17 @@
+// js/dataService.js
+import { CONFIG } from "./config.js";
+import { state } from "./state.js";
+
+export async function loadExcelData() {
+  const { API_KEY, ID, RANGE } = CONFIG.SHEET;
+
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${ID}/values/${RANGE}?key=${API_KEY}`;
+  const res = await fetch(url);
+  const json = await res.json();
+
+  if (!json.values?.length) throw new Error("No data");
+
+  state.allData = json.values.slice(1);
+  state.totalCount = new Set(state.allData.map(r => r[0])).size;
+}
 
